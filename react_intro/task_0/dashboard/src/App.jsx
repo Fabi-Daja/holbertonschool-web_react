51b8f1e4569d02169590cb2dcad49ef4a5c92404
Fabi-Daja <fabjandaja06@gmail.com>
2026-03-12 18:12:38 +0100

    changes

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import image3 from './assets/holberton-logo.jpg';
 import './App.css';
-import logo from './assets/holberton-logo.jpg';
 
-function App() {
+const App = () => {
+  const date = new Date();
   return (
-    <div className="App">
+    <>
       <div className="App-header">
-        <img src={logo} alt="holberton logo" />
-        <h1>School dashboard</h1>
+        <img src={image3} alt='holberton logo' style={{ width: '300px', height: '350px' }} />
+        <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
       </div>
-      <div className="App-body">
+      <div className='App-body'>
         <p>Login to access the full dashboard</p>
       </div>
-      <div className="App-footer">
-        <p>Copyright 2024 - holberton School</p>
+      <div className='App-footer'>
+        <p>Copyright {date.getFullYear()} - holberton School</p>
       </div>
-    </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
